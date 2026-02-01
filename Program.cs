3b087df3fc08860140b71485ba9035c365ed2271
Ilia Zhitenev <iyzhitenev@gmail.com>
2026-02-01 21:11:11 +0100

    setup mysql

--- Program.cs
+++ Program.cs
@@ -1,8 +1,14 @@
 ﻿using ArchieHealthTracker.Bot;
+using ArchieHealthTracker.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var builder = Host.CreateApplicationBuilder(args);
+
+var dbConnectionString = builder.Configuration.GetConnectionString("Database");
+builder.Services.AddDbContext<AppDbContext>(options => options.UseMySql(dbConnectionString, ServerVersion.AutoDetect(dbConnectionString)));
 
 builder.Services.AddHostedService<BotService>();
 
